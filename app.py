9ea996ccc9d50d5559d6ae75eec7422d5f15ebfe
Gustavo Cunha <106495445+cunhagdc@users.noreply.github.com>
2023-07-28 00:11:27 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,42 @@
+import os
+import openai
+from flask import Flask, redirect, render_template, request, url_for
+
+def read_api_key_from_file():
+    with open("config.txt", "r") as file:
+        api_key = file.read().strip()
+    return api_key
+
+api_key = read_api_key_from_file()
+
+app = Flask(__name__)
+
+@app.route("/", methods=("GET", "POST"))
+def index():
+    if request.method == "POST":
+        animal = request.form["animal"]
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=generate_prompt(animal),
+            temperature=0.6,
+            api_key=api_key
+        )
+        return redirect(url_for("index", result=response.choices[0].text))
+
+    result = request.args.get("result")
+    return render_template("index.html", result=result)
+
+def generate_prompt(animal):
+    return """Suggest three names for an animal that is a superhero.
+
+Animal: Cat
+Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
+Animal: Dog
+Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
+Animal: {}
+Names:""".format(
+        animal.capitalize()
+    )
+
+if __name__ == "__main__":
+    app.run()
